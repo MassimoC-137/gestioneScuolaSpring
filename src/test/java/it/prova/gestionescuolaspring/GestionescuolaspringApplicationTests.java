2d4685ffa435fa7e5bed390569339560bb0bb053
MassimoC-137 <massimo.caramanna@gmail.com>
2024-05-17 09:47:45 +0200

    primo commit

---
+++ src/test/java/it/prova/gestionescuolaspring/GestionescuolaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionescuolaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionescuolaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
